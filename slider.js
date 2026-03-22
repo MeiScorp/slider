bccc5799edc04c46a2a6aa77feb15ff7004e8437
Valeriia Barabash <loopaf020@gmail.com>
2026-03-22 15:38:47 +0100

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,54 @@
+const slides = document.querySelectorAll('.slide'),
+      dots = document.querySelectorAll('.dot'),
+      prev = document.getElementById('btn-prev'),
+      next = document.getElementById('btn-next');
+
+let index = 0;
+
+const activeSlide = n => {
+    for (slide of slides) {
+        slide.classList.remove('active');
+    }
+    slides[n].classList.add('active');
+}
+const activeDot = n => {
+    for (dot of dots) {
+        dot.classList.remove('active');
+    }
+    dots[n].classList.add('active');
+}
+const switchDotAndSlide = ind => {
+    activeSlide(ind);
+    activeDot(ind);
+}
+const nextSlide = () => {
+    if(index == slides.length -1) {
+        index = 0;
+        activeSlide(index);
+        activeDot(index);
+    } else {
+        index++;
+        activeSlide(index);
+        activeDot(index);
+    }
+}
+const prevSlide = () => {
+    if(index == 0) {
+        index = slides.length - 1;
+        activeSlide(index);
+        activeDot(index);
+    } else {
+        index--;
+        activeSlide(index);
+        activeDot(index);
+    }
+}
+dots.forEach((item, indexDot) => {
+    item.addEventListener('click', () => {
+        index = indexDot;
+        switchDotAndSlide(index);
+    })
+})
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
